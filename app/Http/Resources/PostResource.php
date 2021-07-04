dba0b6a8eb274d7b3dac1233a1c21727011a21eb
Bilal Wani - GitHub <billal.wani@gmail.com>
2021-07-04 18:26:18 +0530

    added mongodb jessenger support and api basic

---
+++ app/Http/Resources/PostResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PostResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        // return parent::toArray($request);
+        return [
+            "t" => $this->title,
+            "d" => $this->desc,
+            "a" => $this->author
+
+        ];
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +19,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('posts', PostController::class);
