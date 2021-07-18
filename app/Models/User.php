8e81aaeb5335a028ae54474811ddea7c9db7e432
Bilal Wani - GitHub <billal.wani@gmail.com>
2021-07-18 20:53:21 +0530

    breeze ui auth implemented

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,14 +2,26 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+// use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+// use Jenssegers\Mongodb\Eloquent\Model;
 
-class User extends Authenticatable
+use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
+ use Illuminate\Auth\Authenticatable;
+ use Illuminate\Auth\Passwords\CanResetPassword;
+ use Illuminate\Foundation\Auth\Access\Authorizable;
+ use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+ use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+
+class User extends Eloquent implements AuthenticatableContract, AuthorizableContract, CanResetPasswordContract
+
+// class User extends Model implements Authenticatable
 {
     use HasFactory, Notifiable;
+    use Authenticatable, Authorizable, CanResetPassword;
 
     /**
      * The attributes that are mass assignable.
